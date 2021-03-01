6025704f9a1dc9380b09e30d5ec3baa17db65acc
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-03-01 22:24:06 +0530

    added role check and error handler

--- commands_cog.py
+++ commands_cog.py
@@ -6,6 +6,7 @@ class CommandsCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.has_role('Bot mod')
     async def set_channel(self,ctx):
         """
         To set the channel where Zapier sends available internships
@@ -13,9 +14,13 @@ class CommandsCog(commands.Cog):
         """
         if len(ctx.message.channel_mentions)==1:
             await ctx.send('Internships list - ' + ctx.message.channel_mentions[0].mention)
-            print(type(ctx.message.channel_mentions[0]))
         else:
             await ctx.send('''Please mention 1 channel\n@InternshipBot set_channel #channel-name''')
+    
+    @set_channel.error
+    async def set_channel_error(self,ctx,error):
+        if isinstance(error,commands.MissingRole):
+            await ctx.send('\"Bot mod\" role required for command.')
 
 def setup(bot):
     bot.add_cog(CommandsCog(bot))
