6be4653fd993232159fdb380bacd17f5a0649c1c
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-03-01 22:33:45 +0530

    fixed command not working on mobile

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 BOT_USER_ID = os.getenv('BOT_USER_ID')
 
-prefix = f'<@!{BOT_USER_ID}> '
+prefixes = [f'<@!{BOT_USER_ID}> ' , f'<@{BOT_USER_ID}> ']
+prefix = prefixes[0]
 bot = commands.Bot(prefix)
 extensions = ['commands_cog']
 if __name__=='__main__':
@@ -23,6 +24,11 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    await bot.process_commands(message)
+    if message.content.startswith(prefixes[0]) or message.content.startswith(prefixes[1]):
+        message.content = message.content[len(prefixes[0]):] if message.content.startswith(prefixes[0]) else message.content[len(prefixes[1]):]
+        message.content = message.content.split()
+        message.content = ' '.join(message.content)
+        message.content = prefix+message.content
+        await bot.process_commands(message)
 
 bot.run(BOT_TOKEN)
