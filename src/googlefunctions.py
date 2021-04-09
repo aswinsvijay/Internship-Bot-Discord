5d9e90a4d71202e48a303f05d770066929652e0a
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-04-09 21:30:29 +0530

    remove unwanted lines

--- src/googlefunctions.py
+++ src/googlefunctions.py
@@ -41,12 +41,6 @@ async def google_create_form(title, email):
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json',
-                SCOPES
-            )
-            creds = flow.run_local_server(port=0)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     
