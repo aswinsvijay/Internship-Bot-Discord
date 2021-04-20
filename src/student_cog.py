d54bd73d86935a4aada0148b5f39b7d34c473b0e
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-04-20 15:25:17 +0530

    use reaction to get internships list

--- src/student_cog.py
+++ src/student_cog.py
@@ -18,11 +18,19 @@ class StudentCog(commands.Cog, name='Students'):
         """
         To get internships from enrolled guild in DM
         """
+        await ctx.message.add_reaction('🔁')
         internships = await get_internships(ctx.author.id)
         embed = discord.Embed(title='Available internships')
         async for i in internships:
             embed.add_field(name=i[0], value=i[1], inline=False)
-        await ctx.author.send(embed=embed)
+        await ctx.author.send(embed=embed, delete_after=15*60)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user != self.bot.user:
+            message = reaction.message
+            if reaction.emoji == '🔁' and message.author == user:
+                await self.bot.process_commands(message)
 
 def setup(bot):
     bot.add_cog(StudentCog(bot))
